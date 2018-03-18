8cffb1b1ab7a1330035fdef7f1b6b0d2f1300075
SujayHerle <sujayherles@gmail.com>
2018-03-18 13:25:59 +0530

    add connect disocnnet

--- server/server.js
+++ server/server.js
@@ -1,12 +1,24 @@
 const path=require('path');
+const http=require('http');
 const publicPath=path.join(__dirname,'../public');
 const express = require('express');
+const socketIO=require('socket.io');
 
 const port=process.env.PORT||3000;
 var app = express();
-app.use(express.static(publicPath));
+var server=http.createServer(app);
+var io=socketIO(server);
 
-app.listen(port, ()=>{
+app.use(express.static(publicPath));
+io.on('connection',(socket)=>{
+console.log("New connection");
+
+socket.on('disconnect',()=>{
+  console.log("Connection disocnnected")
+});
+});
+
+server.listen(port, ()=>{
   console.log(`Server is up on ${port}`);
 });
 
